140c9e0fd0795475d8f8871898bb218a9908fc83
Knecht27 <alexkophalknecht@gmail.com>
2023-08-30 22:16:00 -0300

    AulaTres

---
+++ Atividades/MultiDimensionalArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿int[,] biNumbers = new int[5,2]; //Matriz bidimensional (bibagual)
+
+int [,,] Trinumbers = new int [5,4,3]; //Matriz tridimensional(tribagual)
+
+int [,] loadedbinumbers = new int [,] 
+{
+    {   9,  5,  8},
+    {  10, -11, 4},
+    { 101, 45, -9}
+};
+
+for(int i = 0; i < loadedbinumbers.GetLength(0); i++)
+{
+    for(int j = 0; j < loadedbinumbers.GetLength(1); j++)
+    {
+        Console.Write(
+            $"[{ loadedbinumbers[i,j]}]"
+            );
+    }
+    Console.WriteLine("");
+}
