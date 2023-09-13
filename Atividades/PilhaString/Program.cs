6d2c9be42096575a160623cc28e2de8de2b2fb77
Knecht27 <alexkophalknecht@gmail.com>
2023-09-13 19:42:19 -0300

    PilhaStack

---
+++ Atividades/PilhaString/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+Stack<string>  PilhaString = new Stack<string>(); //Criar uma pilha de strings
+
+PilhaString.Push("Bom Dia!");
+PilhaString.Push("Guten Morgen!");
+PilhaString.Push("Good Morning!");
+PilhaString.Push("¡Buenos días!");
+
+string topo = PilhaString.Peek ();   // Pegar o elemento no topo da pilha
+Console.WriteLine("Elemento no topo da pilha é: "+ topo );
+
+string desempilhar = PilhaString.Pop ();  //Pega o primeiro dado da pilha e retira ele
+Console.WriteLine("Elemento desempilhado: " + desempilhar );
+
+int tamanho = PilhaString.Count; //Apresenta o tamanho da pilha
+Console.WriteLine("Tamanho desta pilha é: " + tamanho);
+
+Console.WriteLine("Elementos da pilha:"); // Exibe a pilha inteira
+foreach (string elemento in PilhaString)
+{
+    Console.WriteLine(elemento);
+}
