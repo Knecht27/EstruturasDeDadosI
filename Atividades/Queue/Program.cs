195fc90175c0bcdcdeec5b257179544fef40bb68
Knecht27 <alexkophalknecht@gmail.com>
2023-09-25 14:28:06 -0300

    Aula 20/09/23

---
+++ Atividades/Queue/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace QueueClass
+{
+
+    public class Queue
+    {
+
+        static readonly int MAX = 10;
+        int back = -1;
+        int front = -1;
+        int[] queue = new int[MAX];
+
+        public bool IsEmpety(){
+            return (back < 0);
+        }
+
+        public bool Enqueue()
+    }
+}
